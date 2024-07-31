bb963e9f7798f90ad1aaf5d3715bb6b1f03cf629
rajeev-rai-oc <rajeev.rai@oneconvergence.com>
2024-07-31 17:00:46 +0530

    OOps concept

--- OOPs.py
+++ OOPs.py
@@ -44,3 +44,31 @@ obj_rec=Rect(10,8)
 
 for obj in (obj_sq,obj_rec):
     obj.area()
+
+
+
+#Inheritance
+
+# parent class
+class Person():
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def display(self):
+    print(self.name, self.age)
+
+# child class
+class Student(Person):
+  def __init__(self, name, age):
+    self.sName = name
+    self.sAge = age
+    # inheriting the properties of parent class
+    super().__init__("Rajeev", age)
+
+  def displayInfo(self):
+    print(self.sName, self.sAge)
+
+obj = Student("Rajeev", 23)
+obj.display()
+obj.displayInfo()
